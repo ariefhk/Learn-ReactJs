5c51aadd73c081f85e0f8ad7300f386e5ea98a5c
ariefhk <ariefrachmanhakim1001@gmail.com>
2022-10-11 23:44:05 +0700

    fixing roadmap

---
+++ javascript/01-variable-operator-string_conditional/operator.js
@@ -0,0 +1,6 @@
+// 1. Aritmethics
+let tambah = 10 + 5;
+let kurang = 5 - 3;
+let kali = 5 * 7;
+let bagi = 20 / 4;
+let modulus = 22 / 7;
---
+++ javascript/01-variable-operator-string_conditional/variable.js
@@ -0,0 +1,8 @@
+// Variable
+let name = "Arief Rachman Hakim";
+let number = 22;
+let isMale = true;
+
+console.log(`name: ${name}`);
+console.log(`age: ${number}`);
+console.log(`is male: ${isMale}`);
